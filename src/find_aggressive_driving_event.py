a82814a0e1300e1413899944ebc3ffe5a8994671
chenxingqiang <joy6677@qq.com>
2019-03-27 02:53:26 +0800

    debug
    Signed-off-by: chenxingqiang <joy6677@qq.com>

--- src/find_aggressive_driving_event.py
+++ src/find_aggressive_driving_event.py
@@ -7,165 +7,167 @@ from src.config import data_check_path, data_prod_path, \
 
 
 def find_file_dirs(file_dir):
-	"""
+    """
 	:param file_dir:
 	:return:
 	"""
-	dirs_list = []
-	paths = os.listdir(file_dir)
-	
-	for path in paths:
-		dirs_list.append(os.path.join(file_dir,path))
+    dirs_list = []
+    paths = os.listdir(file_dir)
 
-	return dirs_list
+    for path in paths:
+        dirs_list.append(os.path.join(file_dir, path))
+
+    return dirs_list
 
 
-def find_csv_file(csv_path,dotname='csv'):
-	"""
+def find_csv_file(csv_path, dotname='csv'):
+    """
 	:param csv_path:
 	:param dotname:
 	:return:
 	"""
-	L = []
-	for root, dirs, files in os.walk(csv_path):
-		for file in files:
-			if file.split('.')[-1] == dotname:
-				L.append(os.path.join(root, file))
-	if len(L) != 4:
-		print('FILE is NOT Enough!!' * 12)
-		return False,sorted(L)
-	elif len(L) >= 4:
-		return True,sorted(L)
+    L = []
+    for root, dirs, files in os.walk(csv_path):
+        for file in files:
+            if file.split('.')[-1] == dotname:
+                L.append(os.path.join(root, file))
+    if len(L) != 4:
+        print('FILE is NOT Enough!!' * 12)
+        return False, sorted(L)
+    elif len(L) >= 4:
+        return True, sorted(L)
 
 
 def init_path(event_length):
-	"""
+    """
 	:param event_length:
 	:return:
 	"""
-	print(data_prod_path)
-	print(data_check_path)
+    print(data_prod_path)
+    print(data_check_path)
 
-	name_app = os.path.join(root_dir, 'result_event_length_'+str(event_length))
-	print(name_app)
-	if not os.path.exists(name_app):
-		os.makedirs(name_app)
+    name_app = os.path.join(root_dir, 'result_event_length_' + str(event_length))
+    print(name_app)
+    if not os.path.exists(name_app):
+        os.makedirs(name_app)
 
-	return name_app
+    return name_app
 
 
 def data_prepare(name_app):
-	"""
+    """
 	:param name_app:
 	:param event_length:
 	:return:
 	"""
-	
-	# define name parameters
-	data_feature_list = ['Acceleration','Velocity','Steering_Wheel_Angle','Yaw_Rate']
-	alpha_name = ['alpha','0.5']
-	lambda_name = ['lambda','1.5']
 
-	dirs_list = find_file_dirs(data_prod_path)
-	data_results = pd.DataFrame()
+    # define name parameters
+    data_feature_list = ['Acceleration', 'Velocity', 'Steering_Wheel_Angle', 'Yaw_Rate']
+    alpha_name = ['alpha', '0.5']
+    lambda_name = ['lambda', '1.5']
 
-	for dir_name in dirs_list:
-		state_flag,csv_file_path = find_csv_file(dir_name)
-		print(len(csv_file_path))
+    dirs_list = find_file_dirs(data_prod_path)
+    data_results = pd.DataFrame()
 
-		if state_flag:
-			csv_file_path = [x for x in csv_file_path if x.split('/')[-1].split('_')[-3] == alpha_name[-1]]
-			print(csv_file_path)
+    for dir_name in dirs_list:
+        state_flag, csv_file_path = find_csv_file(dir_name)
+        print(len(csv_file_path))
 
-			assert len(csv_file_path) == len(data_feature_list)
+        if state_flag:
+            csv_file_path = [x for x in csv_file_path if x.split('/')[-1].split('_')[-3] == alpha_name[-1]]
+            print(csv_file_path)
 
-			count_flag = 0
-			data_app = pd.DataFrame()
-			for data_path in csv_file_path:
-				feature_name_app = data_path.split('.')[0].split('/')[-1].split('_')[1][0:3]
-				Car_ID = data_path.split('.')[0].split('/')[-1].split('_')[0]
-				data = pd.read_csv(data_path).iloc[:, 1:]
-				data = data.replace('NOT_FULFILLMENT', 0.0)
-				data = data.replace('NOT_FULLFILLMENT', 0.0)
+            assert len(csv_file_path) == len(data_feature_list)
 
-				data['divergence_score'] = data['divergence_score'].astype(float)
+            count_flag = 0
+            data_app = pd.DataFrame()
+            for data_path in csv_file_path:
+                feature_name_app = data_path.split('.')[0].split('/')[-1].split('_')[1][0:3]
+                Car_ID = data_path.split('.')[0].split('/')[-1].split('_')[0]
+                data = pd.read_csv(data_path).iloc[:, 1:]
+                data = data.replace('NOT_FULFILLMENT', 0.0)
+                data = data.replace('NOT_FULLFILLMENT', 0.0)
 
-				data.columns = ['Time', 'divergence_score_' + str(feature_name_app)]
-				data['divergence_score_' + str(feature_name_app)] = data['divergence_score_' + str(
-					feature_name_app)].abs() * 1000
-				
-				if count_flag == 0:
-					data_app = data
-					count_flag = 1
-				else:
-					data_app = pd.merge(data_app, data, on=['Time'], how='right')
+                data['divergence_score'] = data['divergence_score'].astype(float)
 
-				# print(data.head())
+                data.columns = ['Time', 'divergence_score_' + str(feature_name_app)]
+                data['divergence_score_' + str(feature_name_app)] = data['divergence_score_' + str(
+                    feature_name_app)].abs() * 1000
 
-			data_orig = pd.read_csv(os.path.join(data_check_path, Car_ID, dir_name.split('/')[-1]+'.csv'))
-			data_result = pd.merge(data_orig, data_app, on=['Time'], how='left')
-			# standard of acd
-			
-			data_result['ds_total'] = data_result['divergence_score_Acc']+data_result['divergence_score_Yaw']+data_result['divergence_score_Vel'] + data_result['divergence_score_Ste']
-			
-			data_results = data_results.append(data_result)
+                if count_flag == 0:
+                    data_app = data
+                    count_flag = 1
+                else:
+                    data_app = pd.merge(data_app, data, on=['Time'], how='right')
 
-			if len(data_results) >= event_length:
-				top5_ds = find_top5(data_results)
-				print('FINAL THRESHOLD IS:: ', top5_ds)
+            # print(data.head())
 
-		else:
-			continue
+            data_orig = pd.read_csv(os.path.join(data_check_path, Car_ID, dir_name.split('/')[-1] + '.csv'))
+            data_result = pd.merge(data_orig, data_app, on=['Time'], how='left')
+            # standard of acd
 
-	data_results['is_acp'] = data_results['ds_total'].map(lambda x: 1 if x >= top5_ds else 0)
-	
-	data_results = data_results.reset_index().drop(['index'],axis=1)
-	data_results.to_csv(os.path.join(name_app,'drive_event_result.csv'))
-	print('***** SAVING ORIGINAL EVENT DATA AS '+os.path.join(name_app,'drive_event_result.csv')+'  *****')
-	save_event(data_results,name_app)
+            data_result['ds_total'] = data_result['divergence_score_Acc'] + data_result['divergence_score_Yaw'] + \
+                                      data_result['divergence_score_Vel'] + data_result['divergence_score_Ste']
+
+            data_results = data_results.append(data_result)
+
+            if len(data_results) >= event_length:
+                top5_ds = find_top5(data_results)
+                print('FINAL THRESHOLD IS:: ', top5_ds)
+
+        else:
+            continue
+
+    data_results['is_acp'] = data_results['ds_total'].map(lambda x: 1 if x >= top5_ds else 0)
+
+    data_results = data_results.reset_index().drop(['index'], axis=1)
+    data_results.to_csv(os.path.join(name_app, 'drive_event_result.csv'))
+    print('***** SAVING ORIGINAL EVENT DATA AS ' + os.path.join(name_app, 'drive_event_result.csv') + '  *****')
+    save_event(data_results, name_app)
 
 
 def find_top5(data_result):
-	"""
+    """
 	:param data_result:
 	:return:
 	"""
-	assert len(data_result) >= event_length
+    assert len(data_result) >= event_length
 
-	data_result = data_result.sort_values(by=['ds_total'],ascending=False)
-	data_result = data_result.reset_index().drop(['index'],axis=1).fillna(0.0)
+    data_result = data_result.sort_values(by=['ds_total'], ascending=False)
+    data_result = data_result.reset_index().drop(['index'], axis=1).fillna(0.0)
 
-	top5 = int(0.05*len(data_result))
-	print('Event Length {0} Top 5 index is {1} ,and real data length of estimating is {2} '.format(event_length,top5,len(data_result)))
+    top5 = int(0.05 * len(data_result))
+    print('Event Length {0} Top 5 index is {1} ,and real data length of estimating is {2} '.format(event_length, top5,
+                                                                                                   len(data_result)))
 
     top5_ds = data_result.iloc[top5, -1]
-	print('top5 divergence_score ', top5_ds)
+    print('top5 divergence_score ', top5_ds)
 
-	return top5_ds
+    return top5_ds
 
 
-def save_event(data,name_app):
-	"""
+def save_event(data, name_app):
+    """
 	:param data:
 	:param name_app:
 	:return:
 	"""
-	data_re = data[data.is_acp == 1]
-	events_index = [[int(x)-(n-1), int(x)+1]for x in list(data_re.index)]
-	event_df = pd.DataFrame(events_index)
-	event_df.columns = ['start_index', 'end_index']
+    data_re = data[data.is_acp == 1]
+    events_index = [[int(x) - (n - 1), int(x) + 1] for x in list(data_re.index)]
+    event_df = pd.DataFrame(events_index)
+    event_df.columns = ['start_index', 'end_index']
 
-	event_df.to_csv(os.path.join(name_app, 'drive_event_index.csv'))
-	print('*****  SAVING DRIVE EVENT INDEX AS '+os.path.join(name_app,'drive_event_index.csv')+' *****')
+    event_df.to_csv(os.path.join(name_app, 'drive_event_index.csv'))
+    print('*****  SAVING DRIVE EVENT INDEX AS ' + os.path.join(name_app, 'drive_event_index.csv') + ' *****')
 
 
 def find_event():
-	"""
+    """
 	:return:
 	"""
-	name_app = init_path(event_length=event_length)
-	data_prepare(name_app)
+    name_app = init_path(event_length=event_length)
+    data_prepare(name_app)
 
 
 if __name__ == '__main__':
-	find_event()
+    find_event()
